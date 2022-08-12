c50caa48944bc05dff4b2e2b6146b5d1c99806cf
soldierrogers <111144269+soldierrogers@users.noreply.github.com>
2022-08-12 19:59:02 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,39 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Wonders</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+ 
+{block:Video}{Video-500}{block:Caption}{Caption}{/block:Caption}{/block:Video}
+ 
+{block:Answer}<div id="asker">{Question} —<span style="text-align:lowercase; color:#fcfcfc;font-style:italic; "><b><i>{Asker}</i></b><br></span> </div><left><div style="padding:4px;margin-top:3px;font-size:11px;"><font face="arial">{Answer}</font></div>{/block:Answer}
+   
+<div id="info">
+<a href="{Permalink}">{Month} {DayOfMonth}{DayOfMonthSuffix}</a> —
+{block:RebloggedFrom} <a href="{ReblogParentURL}">via</a>{/block:RebloggedFrom}
+and {block:ContentSource} <a href="{SourceURL}"><a href="{SourceURL}">source</a>{/block:ContentSource}
+{block:RebloggedFrom} <a href="{ReblogParentURL}" target="_blank"></a>{/block:RebloggedFrom}
+with {NoteCountWithLabel}</a>
+{block:HasTags}<div id="tags">
+ 
+{block:Tags}<a href="/tagged/{Tag}">#{Tag}</a>&nbsp;{/block:Tags}
+</div>{block:HasTags}
+</div>
+</div>
+{/block:Posts}
+<br><br>{block:PostNotes}<div id="notes"><right>{PostNotes}</div>{/block:PostNotes}<right>
+{/block:Posts}</div></div></div>
+<br><br><br><br>
+   
+<div id="cred"><a href="http://screwirwin.tumblr.com">♥ SCREWIRWIN ♥</a></div>    
+</div>
+</div>
+ 
+ 
+       <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+      
+<script type="text/javascript">
+window.onload = function() {
+    $('iframe.photoset').contents().find('.photoset_row').attr("style", "width:500px; margin-bottom: -4px;");
+    $('iframe.photoset').contents().find('.photoset_row').find('img').attr("style", "width:500px;");
+    $('iframe.photoset').contents().find('.photoset_row_2').find('img').attr("style", "width:245px; margin-right: 0px;");
+    $('iframe.photoset').contents().find('.photoset_row_3').find('img').attr("style", "width:160px; margin-right: 0px;");
 }
+</script>
+ <script src=”http://static.tumblr.com/2w7y46r/xtmlvfnoc/snowstorm.js”></script>
+    </body></html>
